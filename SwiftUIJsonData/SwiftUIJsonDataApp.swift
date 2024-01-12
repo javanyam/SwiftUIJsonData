b96e8ae74e40090c0d8476da71e240e82a773ca3
javanyam <yjlee@snaptag.co.kr>
2024-01-12 17:17:18 +0900

    Initial Commit

---
+++ SwiftUIJsonData/SwiftUIJsonDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJsonDataApp.swift
+//  SwiftUIJsonData
+//
+//  Created by SNAPTAG on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJsonDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
